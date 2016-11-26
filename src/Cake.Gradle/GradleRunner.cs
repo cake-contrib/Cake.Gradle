7386b46644739fadc9419443adc3cd61e52deb8a
abeggchr <abegg.ch@gmail.com>
2016-11-26 20:18:35 +0100

    added basic addin classes

---
+++ src/Cake.Gradle/GradleRunner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Gradle
+{
+    public class GradleRunner : Tool<GradleRunnerSettings>
+    {
+        /// <summary>
+        /// Get a Gradle runner
+        /// </summary>
+        public GradleRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner, IToolLocator tools, Verbosity logVerbosity) : base(fileSystem, environment, processRunner, tools)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetToolName()
+        {
+            return "Gradle Runner";
+        }
+
+        /// <summary>
+        /// Gets the name of the tool executable
+        /// </summary>
+        /// <returns>The tool executable name</returns>
+        protected override IEnumerable<string> GetToolExecutableNames()
+        {
+            yield return "gradlew";
+            yield return "gradlew.bat";
+        }
+    }
+}
---
+++ src/Cake.Gradle/GradleRunnerAlias.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+
+namespace Cake.Gradle
+{
+    [CakeAliasCategory("Npm")]
+    public static class GradleRunnerAlias
+    {
+
+        [CakePropertyAlias]
+        public static GradleRunner Gradle(this ICakeContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            return new GradleRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools, context.Log.Verbosity);
+        }
+    }
+}
