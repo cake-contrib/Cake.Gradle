9de72d7e278506b6befdc4b37ff9514b0b9e42c8
GitHub <noreply@github.com>
2021-02-22 07:55:19 +0000

    Continuous Integration Publish: 10b6f5d325fe26731e9d90b9e7911743bacdfc0c Merge pull request #61 from cake-contrib/dependabot/nuget/develop/coverlet.msbuild-3.0.3

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,14 +39,14 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"GradleRunnerAlias",
-            content:"GradleRunnerAlias",
+            title:"GradleRunner",
+            content:"GradleRunner",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.Gradle/api/Cake.Gradle/GradleRunnerAlias',
-            title:"GradleRunnerAlias",
+            url:'/Cake.Gradle/api/Cake.Gradle/GradleRunner',
+            title:"GradleRunner",
             description:""
         }
     );
@@ -67,6 +67,20 @@ var searchModule = function() {
     a(
         {
             id:2,
+            title:"GradleRunnerAlias",
+            content:"GradleRunnerAlias",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Gradle/api/Cake.Gradle/GradleRunnerAlias',
+            title:"GradleRunnerAlias",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
             title:"GradleLogLevel",
             content:"GradleLogLevel",
             description:'',
@@ -75,20 +89,6 @@ var searchModule = function() {
         {
             url:'/Cake.Gradle/api/Cake.Gradle/GradleLogLevel',
             title:"GradleLogLevel",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
-            title:"GradleRunner",
-            content:"GradleRunner",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Gradle/api/Cake.Gradle/GradleRunner',
-            title:"GradleRunner",
             description:""
         }
     );
