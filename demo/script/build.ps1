27e39224c75eafb2c22e302a2ff0f6651eed9fc1
Nils Andresen <nils@nils-andresen.de>
2025-05-29 21:08:15 +0200

    updated demos

--- demo/script/build.ps1
+++ demo/script/build.ps1
@@ -6,6 +6,10 @@ $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
+# ensure a most-recent debug-build, so we can reference that.
+dotnet build ../../src/Cake.Gradle/Cake.Gradle.csproj
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 dotnet tool restore
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
--- demo/script/build.sh
+++ demo/script/build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-set -euox pipefail
+set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
+
+# ensure a most-recent debug-build, so we can reference that.
+dotnet build ../../src/Cake.Gradle/Cake.Gradle.csproj
 
 dotnet tool restore
 
